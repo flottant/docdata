666d8956bd9eda3d76105f5de82adc58b4a6e28b
flottant <flottant_official@outlook.com>
2022-04-23 23:53:25 +0800

    change github edit config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/linyuxuanlin/Wiki_Docusaurus/edit/main/',
+          editUrl: 'https://github.com/flottant/docdata/edit/main/',
           sidebarCollapsible: true, //默认折叠
           routeBasePath: "/",
           showLastUpdateTime: true,
